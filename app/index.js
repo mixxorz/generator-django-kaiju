9fb6fd2c585fc39ebff5bb3f15914afa34823eb2
mixxorz <mixxorz@gmail.com>
2014-10-23 12:42:27 +0800

    Started dev. Added first prompt

--- app/index.js
+++ app/index.js
@@ -5,50 +5,51 @@ var yeoman = require('yeoman-generator');
 var yosay = require('yosay');
 
 var DjangoKaijuGenerator = yeoman.generators.Base.extend({
-  initializing: function () {
-    this.pkg = require('../package.json');
-  },
-
-  prompting: function () {
-    var done = this.async();
-
-    // Have Yeoman greet the user.
-    this.log(yosay(
-      'Welcome to the flawless DjangoKaiju generator!'
-    ));
-
-    var prompts = [{
-      type: 'confirm',
-      name: 'someOption',
-      message: 'Would you like to enable this option?',
-      default: true
-    }];
-
-    this.prompt(prompts, function (props) {
-      this.someOption = props.someOption;
-
-      done();
-    }.bind(this));
-  },
-
-  writing: {
-    app: function () {
-      this.dest.mkdir('app');
-      this.dest.mkdir('app/templates');
-
-      this.src.copy('_package.json', 'package.json');
-      this.src.copy('_bower.json', 'bower.json');
+    initializing: function() {
+        this.pkg = require('../package.json');
+        this.projectName = process.cwd().split(path.sep).pop();
     },
 
-    projectfiles: function () {
-      this.src.copy('editorconfig', '.editorconfig');
-      this.src.copy('jshintrc', '.jshintrc');
-    }
-  },
+    prompting: function() {
+        var done = this.async();
 
-  end: function () {
-    this.installDependencies();
-  }
+        // Have Yeoman greet the user.
+        this.log(yosay(
+            'Yo Kaiju!'
+        ));
+
+        var prompts = [{
+            type: 'input',
+            name: 'projectName',
+            message: 'What\'s the name of your project?',
+            default: this.projectName
+        }];
+
+        this.prompt(prompts, function(props) {
+            this.projectName = props.projectName;
+
+            done();
+        }.bind(this));
+    },
+
+    writing: {
+        app: function() {
+            this.dest.mkdir('app');
+            this.dest.mkdir('app/templates');
+
+            this.src.copy('_package.json', 'package.json');
+            this.src.copy('_bower.json', 'bower.json');
+        },
+
+        projectfiles: function() {
+            this.src.copy('editorconfig', '.editorconfig');
+            this.src.copy('jshintrc', '.jshintrc');
+        }
+    },
+
+    end: function() {
+        this.installDependencies();
+    }
 });
 
 module.exports = DjangoKaijuGenerator;
