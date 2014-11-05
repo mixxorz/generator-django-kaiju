0555f29f7d85d21306c33901145969b39ed59f11
mixxorz <mixxorz@gmail.com>
2014-11-05 17:47:40 +0800

    Fixed post compile script templates

--- app/index.js
+++ app/index.js
@@ -80,12 +80,23 @@ var DjangoKaijuGenerator = yeoman.generators.Base.extend({
             this.template('Procfile', 'Procfile');
             this.src.copy('requirements.txt', 'requirements.txt');
             this.dest.mkdir('bin');
-            // this.template('bin/cleanup', 'bin/cleanup');
-            // this.template('bin/compile_assets', 'bin/compile_assets');
+            this.template('bin/cleanup', 'bin/cleanup');
+            this.template('bin/compile_assets', 'bin/compile_assets');
             this.src.copy('bin/install_nodejs', 'bin/install_nodejs');
             this.src.copy('bin/install_npm_packages', 'bin/install_npm_packages');
             this.src.copy('bin/post_compile', 'bin/post_compile');
-            // this.template('bin/run_collectstatic', 'bin/run_collectstatic');
+
+            // This particular file requires us to change the underscore tags
+            // to {{ }}
+            this.template(
+                'bin/run_collectstatic',
+                'bin/run_collectstatic',
+                this, {
+                    evaluate: /\{\{([\s\S]+?)\}\}/g,
+                    interpolate: /\{\{=([\s\S]+?)\}\}/g,
+                    escape: /\{\{-([\s\S]+?)\}\}/g
+                }
+            );
         }
         // app: function() {
         //     this.dest.mkdir('app');
